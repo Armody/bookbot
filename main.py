5644bae18060e96f8409555b6b458a097bb28c4a
Armody <Rmdyzie@gmail.com>
2024-08-29 16:18:06 +0300

    added individual character count

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     amount = get_number_of_words(text)
+    character_count = each_character_used(text)
     print(amount)
+    print(character_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -13,5 +15,14 @@ def get_number_of_words(text):
     for word in text.split():
         amount += 1
     return amount
+
+def each_character_used(text):
+    character_count = {}
+    for character in text.lower():
+        if character not in character_count:
+            character_count[character] = 1
+        else:
+            character_count[character] += 1
+    return character_count
 
 main()
