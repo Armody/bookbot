90aeab80b3390f0bb2332e599f3ea577f12d14c8
Armody <Rmdyzie@gmail.com>
2024-08-29 19:01:30 +0300

    added sorting of characters by amount in text

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ def main():
     text = get_book_text(book_path)
     amount = get_number_of_words(text)
     character_count = each_character_used(text)
-    
+    sorted_characters = sort_characters_by_amount(character_count)
     print(f"--- Begin report of {book_path} ---")
     print(f"{amount} words found in the document")
-    print(character_count)
+    for dict in sorted_characters:
+        character = dict["character"]
+        value = dict["value"]
+        print(f"The '{character}' character was found {value} times")
     print("--- End report ---")
 
 def get_book_text(path):
@@ -28,7 +31,20 @@ def each_character_used(text):
             character_count[character] += 1
     return character_count
 
-#def sort_characters_by_amount(character):
+def sort_characters_by_amount(characters):
+    characters_list = []
+    ## Convert to list of dictionaries
+    for character in characters:
+        if character.isalpha():
+            temp = {}
+            temp["character"] = character
+            temp["value"] = characters[character]
+            characters_list.append(temp)
+    ## Sorts
+    characters_list.sort(key=sort_on, reverse=True) 
+    return characters_list
 
+def sort_on(dict):
+    return dict["value"]
 
 main()
